d935d9e4bef2d7d5a535b72ade9bca0113c9bad3
QusaiMjaly <102429319+QusaiMjaly@users.noreply.github.com>
2025-12-17 19:09:31 +0200

    Update CoinDecisionTest.java

--- bva-desciisionTable/src/test/java/bva_desciisionTable/bva_desciisionTable/CoinDecisionTest.java
+++ bva-desciisionTable/src/test/java/bva_desciisionTable/bva_desciisionTable/CoinDecisionTest.java
@@ -1,5 +1,38 @@
 package bva_desciisionTable.bva_desciisionTable;
 
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class CoinDecisionTest {
 
+    @Test
+    void R1_TTTT() {
+        CoinDecision cd = new CoinDecision();
+        assertEquals("Negative", cd.CoinDecision("T","T","T","T"));
+    }
+
+    @Test
+    void R2_HTTT() {
+        CoinDecision cd = new CoinDecision();
+        assertEquals("Negative", cd.CoinDecision("H","T","T","T"));
+    }
+
+    @Test
+    void R3_HHTT() {
+        CoinDecision cd = new CoinDecision();
+        assertEquals("Positive", cd.CoinDecision("H","H","T","T"));
+    }
+
+    @Test
+    void R4_HHHT() {
+        CoinDecision cd = new CoinDecision();
+        assertEquals("Positive", cd.CoinDecision("H","H","H","T"));
+    }
+
+    @Test
+    void R5_HHHH() {
+        CoinDecision cd = new CoinDecision();
+        assertEquals("Positive", cd.CoinDecision("H","H","H","H"));
+    }
 }
